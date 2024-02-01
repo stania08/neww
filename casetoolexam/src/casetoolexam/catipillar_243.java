04eef2b6ad45e7f6e83bd04e582a27ad0c6de073
stania rebelo <staniarebelo08@gmail.com>
2024-02-01 10:29:36 +0530

    gfj

---
+++ casetoolexam/src/casetoolexam/catipillar_243.java
@@ -0,0 +1,13 @@
+package casetoolexam;
+
+public class catipillar_243 {
+	
+	int moves,cabbages;
+	
+	catipillar_243(int moves,int cabbages)
+	{
+		this.moves=moves;
+		this.cabbages=cabbages;
+	}
+
+}
